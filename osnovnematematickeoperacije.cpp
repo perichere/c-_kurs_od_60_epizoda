f52244bb6e72bd7e375940c972fdfabbb4533a98
perichere <77920541+perichere@users.noreply.github.com>
2021-02-24 04:32:56 +0100

    Create osnovnematematickeoperacije.cpp

---
+++ osnovnematematickeoperacije.cpp
@@ -0,0 +1,24 @@
+#include  <iostream>
+
+using namespace std;
+
+int main() {
+  int a = 80;
+  int b = 20;
+  int proizvod;
+  int kolicnik;
+  
+  proizvod = a * b;
+  
+  cout << proizvod << endl;
+  
+  kolicnik = a / b;
+  
+  cout << kolicnik << endl;
+  
+  int ostatak = a % b;
+  
+  cout << ostatak << endl;
+  
+  return 0;
+}
