8fa08b4ed4bfc69d580d043b021e7268ed39aec6
perichere <77920541+perichere@users.noreply.github.com>
2021-02-24 12:52:12 +0000

    Create grananje(if).cpp

---
+++ grananje(if).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int broj = 5;
+  if (broj < 3) {
+    cout << "Broj je manji od 3" << endl;
+  }
+  if (broj > 3) {
+    cout << "Broj je veći od 3" << endl;
+  }
+
+
+return 0;
+
+}
