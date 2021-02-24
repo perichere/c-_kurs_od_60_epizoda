d546464ad4eea24e3591c2b5ef3fa47ffd5a5053
perichere <77920541+perichere@users.noreply.github.com>
2021-02-24 04:33:51 +0100

    Create unosprekotastature.cpp

---
+++ unosprekotastature.cpp
@@ -0,0 +1,19 @@
+#include  <iostream>
+
+using namespace std;
+
+int main() {
+  
+  int pb;
+  int db;
+  int zbir;
+  cout << "Unesi prvi broj: " << endl;
+  cin >> pb;
+  cout << "Unesite drugi broj: " << endl;
+  cin >> db;
+  zbir = pb + db;
+  cout << "###########################" << endl;
+  cout << "Zbir je " << zbir << endl;
+  
+  return 0;
+}
