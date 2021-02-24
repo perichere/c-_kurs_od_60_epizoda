a3d187903bd56beab64b8475a431a15869652d8b
perichere <77920541+perichere@users.noreply.github.com>
2021-02-24 12:42:37 +0000

    Create osnovnitipovipodataka.cpp

---
+++ osnovnitipovipodataka.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a = 5;
+  
+  float b = 43.43;
+  
+  double c = 4323.4323;
+  
+  char karakter = 'A';
+  
+  cout << b << endl;
+  
+  cout << c << endl;
+  
+  cout << a << endl;
+  
+  cout << karakter << endl;
+  
+  
+  
+
+  return 0;
+
+}
