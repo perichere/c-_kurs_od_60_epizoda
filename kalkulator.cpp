15cc2bc7fb5eb130ef0679995c06f31067abd9c2
perichere <77920541+perichere@users.noreply.github.com>
2021-02-24 04:31:44 +0100

    Create kalkulator.cpp

---
+++ kalkulator.cpp
@@ -0,0 +1,28 @@
+#include  <iostream>
+using namespace std;
+
+int main() {
+  
+  int prvi_broj = 5;
+  int drugi_broj = 20;
+  
+  int zbir = prvi_broj + drugi_broj;
+  
+  cout << zbir << endl;
+  
+  return 0;
+}
+
+drugi nacin je
+#include  <iostream>
+
+using namespace std;
+
+int main() {
+
+  int zbir = 5 + 3 + 4 + 6 + 9;
+  
+  cout << zbir << endl;
+  
+  return 0;
+}
