e7aedad261111ac78e01830a8b5e4f693fd508f6
perichere <77920541+perichere@users.noreply.github.com>
2021-02-24 04:30:37 +0100

    Create headeriprviprogram.cpp

---
+++ headeriprviprogram.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()  {
+  
+  cout << "##########################################################" << endl;
+  cout << "# Cao svima kako ste šta radite ovov je program napravljen od strane Nikole Perića " << endl;
+  cout << "__________________________________________________________" << endl;
+  cout << "#" << endl;
+  cout << "#" << endl;
+  cout << "# Dobrodosli u nas klub debelih igrača" << endl;
+  cout << "**********************************************************" << endl;
+  cout << "# Ovde nećete naći ništa normalno ########################" << endl;
+    return 0;
+}
